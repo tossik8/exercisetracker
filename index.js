dacde47dc08c1c419cc05e7c6b3a48c9393febeb
unknown <toropov.nikita2004@gmail.com>
2023-04-09 12:13:53 +0200

    feat: add new users

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const uuid = require("uuid");
+
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+app.use(express.urlencoded({extended: false}));
 
 
 
@@ -15,4 +18,12 @@ app.get('/', (req, res) => {
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
+})
+
+const usersLog = [];
+
+app.post("/api/users", (req, res) => {
+  const id = uuid.v4();
+  usersLog.push({"_id": id, "username": req.body.username, "count": 0, "log":[]});
+  res.json({"username": req.body.username, "_id": id});
 })
