8aee4eb6bf52fc4e19b4115118adb6752d6ba159
unknown <toropov.nikita2004@gmail.com>
2023-04-09 12:41:05 +0200

    feat: add exercises to the log

--- index.js
+++ index.js
@@ -26,4 +26,36 @@ app.post("/api/users", (req, res) => {
   const id = uuid.v4();
   usersLog.push({"_id": id, "username": req.body.username, "count": 0, "log":[]});
   res.json({"username": req.body.username, "_id": id});
+});
+app.post("/api/users/:id/exercises", (req, res) => {
+  let {description, duration, date } = req.body;
+  const id = req.body[":_id"];
+  let isPresent = false;
+  for(let record of usersLog){
+    if(record._id === id){
+      isPresent = true;
+      break;
+    }
+  }
+  if(isPresent){
+    if(date === "") date = new Date(Date.now());
+    else{
+      date = new Date(date);
+    }
+    let username;
+    for(let log of usersLog){
+      if(log._id === id){
+        log.log.push({"description": description, "duration": duration, "date": date});
+        username = log.username;
+        ++log.count;
+        console.log(log);
+        break;
+      }
+    }
+    res.json({"_id": id, "username": username, "date": date, "duration": duration, "description": description});
+  }
+  else{
+    res.send("Could not find a user with id: " + id)
+  }
+
 })
